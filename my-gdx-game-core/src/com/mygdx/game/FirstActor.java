6450182a78dc709dd89737ba4b085489bfb4c375
Luci3en <36213361+Luci3en@users.noreply.github.com>
2018-03-18 19:08:28 +0100

    Input code
    using different Input options

--- my-gdx-game-core/src/com/mygdx/game/FirstActor.java
+++ my-gdx-game-core/src/com/mygdx/game/FirstActor.java
@@ -3,55 +3,50 @@ package com.mygdx.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
-import com.badlogic.gdx.scenes.scene2d.actions.RotateToAction;
-import com.badlogic.gdx.scenes.scene2d.actions.ScaleToAction;
 
-public class FirstActor extends Actor {
+public class FirstActor {
 
 	private Texture texture;
+	private float x, y;
 
 	public FirstActor() {
 		super();
 		texture = new Texture(Gdx.files.internal("img/player.png"));
+		this.x = 0;
+		this.y = 0;
 
-		MoveToAction action = new MoveToAction();
-		action.setPosition(750, 250);
-		action.setDuration(10f);
-
-		ScaleToAction scaleToAction = new ScaleToAction();
-
-		scaleToAction.setScale(4, 17);
-		scaleToAction.setDuration(10f);
-
-		RotateToAction rotateToAction = new RotateToAction();
-		rotateToAction.setDuration(10f);
-		rotateToAction.setRotation(100f);
-
-		addAction(action);
-		addAction(rotateToAction);
-		addAction(scaleToAction);
 	}
 
-	@Override
 	public void draw(Batch batch, float parentAlpha) {
-		batch.draw(texture, this.getX(), getY(), this.getOriginX(), this.getOriginY(), texture.getHeight(),
-				texture.getWidth() , this.getScaleX(), this.getScaleY(), this.getRotation(), 0, 0, texture.getWidth(),
-				texture.getHeight(), false, false);
-		
-		
-		
-	}
 
-	@Override
-	public void act(float delta) {
-		super.act(delta);
-
-		for (int i = 0; i < this.getActions().size; i++) {
-			getActions().get(i).act(delta);
-		}
+		batch.draw(texture, x, y);
 
 	}
+
+	public Texture getTexture() {
+		return texture;
+	}
+
+	public void setTexture(Texture texture) {
+		this.texture = texture;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+	
+	
 
 }
--- my-gdx-game-core/src/com/mygdx/game/GameScreen.java
+++ my-gdx-game-core/src/com/mygdx/game/GameScreen.java
@@ -1,30 +1,60 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
-public class GameScreen implements Screen {
+public class GameScreen implements Screen, InputProcessor {
 
 	private Stage stage;
-
+	private TiledMap map;
+	private OrthogonalTiledMapRenderer mapRendere;
+	private FirstActor player;
 
 	@Override
 	public void show() {
+		
 		stage = new Stage();
-		Gdx.input.setInputProcessor(stage);
-
+		InputMultiplexer multiplexer = new InputMultiplexer();
+		Gdx.input.setInputProcessor(multiplexer);
+		multiplexer.addProcessor(this);
+		multiplexer.addProcessor(stage);
 		
 		
-		FirstActor actor = new FirstActor();
-		actor.setPosition(25, 25);
-		actor.setOrigin(25, 25);
+		
 	
+		map = new TmxMapLoader().load("maps/map.tmx");
+		mapRendere = new OrthogonalTiledMapRenderer(map);
+
+		player = new FirstActor();
 		
-		stage.addActor(actor);
-		
-		
+		stage.addListener(new InputListener() {
+			
+			public boolean keyDown(com.badlogic.gdx.scenes.scene2d.InputEvent event, int keycode) {
+				
+				
+				if(keycode == Input.Keys.O) {
+					Gdx.app.log("show()", "set inputprocessor for stage KEY O");
+				}
+				return false;
+				
+			};
+		}
+				
+				
+				
+				
+				);
+
 	}
 
 	@Override
@@ -49,9 +79,56 @@ public class GameScreen implements Screen {
 	public void render(float arg0) {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		if (Gdx.input.isKeyPressed(Input.Keys.K))
+
+		{
+			map.getLayers().get(0).setVisible(false);
+		}
 		
-		stage.act(Gdx.graphics.getDeltaTime());
-		stage.draw();
+		
+		movePlayer();
+		
+		stage.getCamera().position.x = player.getX();
+		stage.getCamera().position.y = player.getY();
+		stage.getCamera().update();
+		stage.getBatch().setProjectionMatrix(stage.getCamera().combined);
+
+		mapRendere.setView((OrthographicCamera) stage.getCamera());
+		mapRendere.render();
+
+		stage.getBatch().begin();
+
+		player.draw(stage.getBatch(), 1);
+
+		stage.getBatch().end();
+
+
+	}
+
+	public void movePlayer() {
+
+		if (Gdx.input.isKeyPressed(Input.Keys.U)) {
+			Gdx.app.log("movePlayer()", "static usage of inputprocessor KEY U");
+		}
+		
+		
+		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			player.setY(player.getY() + 2);
+		}
+
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			player.setY(player.getY() - 2);
+		}
+
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			player.setX(player.getX() - 2);
+		}
+
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			player.setX(player.getX() + 2);
+		}
+
 	}
 
 	@Override
@@ -64,6 +141,59 @@ public class GameScreen implements Screen {
 	public void resume() {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public boolean keyDown(int arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keyCode) {
+		
+		if(keyCode == Input.Keys.I) {
+			Gdx.app.log("keyUp()", "using InputProcessor Interface KEY I");
+		}
+		
+		
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int arg0, int arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int arg0, int arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
