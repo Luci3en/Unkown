2d316fea85c02cc3d908d6c3292426b24f239a53
Luci3n <lukasries@t-online.de>
2018-03-26 13:51:50 +0200

    camera changes
    add viewport, and clamp

--- my-gdx-game-core/src/screen/GameScreen.java
+++ my-gdx-game-core/src/screen/GameScreen.java
@@ -1,7 +1,8 @@
 package screen;
 
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
@@ -11,8 +12,8 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.utils.viewport.FillViewport;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.Player;
 import com.mygdx.game.TileMap;
@@ -20,11 +21,11 @@ import com.mygdx.game.TileMap;
 public class GameScreen implements Screen {
 
 	public static int VIEWPORT_WIDTH = 800;
-	public static int VIEWPORT_HEIGHT = 480;
+	public static int VIEWPORT_HEIGHT = 800;
 	private OrthographicCamera camera;
-	private Viewport viewport;
-	private SpriteBatch batch;
+	private SpriteBatch spriteBatch;
 	private OrthogonalTiledMapRenderer tiledMapRenderer;
+	private Viewport viewport;
 	private AssetManager assetManager;
 	private TileMap map;
 	private Player player;
@@ -36,44 +37,38 @@ public class GameScreen implements Screen {
 		assetManager.load("img/player.png", Texture.class);
 		assetManager.finishLoading();
 
-		this.batch = new SpriteBatch();
+		this.spriteBatch = new SpriteBatch();
 
 		this.map = new TileMap();
 		this.tiledMapRenderer = new OrthogonalTiledMapRenderer(map.getMap());
 
-		float aspectRatio = (float) Gdx.graphics.getHeight() / (float) Gdx.graphics.getWidth();
-
-		camera = new OrthographicCamera(TileMap.MAP_PIXEL_WIDTH * aspectRatio, TileMap.MAP_PIXEL_HEIGHT);
-		
-//		viewport = new FillViewport(TileMap.MAP_PIXEL_WIDTH * aspectRatio, TileMap.MAP_PIXEL_HEIGHT);
-//		viewport.apply();
+		this.camera = new OrthographicCamera();
+		this.viewport = new StretchViewport(GameScreen.VIEWPORT_WIDTH, GameScreen.VIEWPORT_HEIGHT, camera);
+		viewport.apply();
 		camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
-		camera.update();
 
 		this.player = new Player(new Sprite(assetManager.get("img/player.png", Texture.class)),
 				(TiledMapTileLayer) map.getMap().getLayers().get("Kachelebene 2"));
 
-		InputMultiplexer multiplexer = new InputMultiplexer();
-		Gdx.input.setInputProcessor(multiplexer);
-		multiplexer.addProcessor(player);
+		Gdx.input.setInputProcessor(player);
+		// InputMultiplexer multiplexer = new InputMultiplexer();
+		// multiplexer.addProcessor(player);
 
 	}
 
 	@Override
 	public void dispose() {
 		assetManager.dispose();
-		batch.dispose();
+		spriteBatch.dispose();
+		player.getTexture().dispose();
 
 	}
-	
 
 	@Override
 	public void resize(int width, int height) {
-		//viewport.update(width, height);
-		camera.viewportHeight = height;
-		camera.viewportWidth = width;
+		viewport.update(width, height);
 		camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
-		
+
 	}
 
 	@Override
@@ -93,7 +88,32 @@ public class GameScreen implements Screen {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		camera.translate(player.getVelocity().x, player.getVelocity().y, 0);
+		camera.position.x = MathUtils.clamp(camera.position.x, camera.viewportWidth / 2,
+				TileMap.MAP_PIXEL_WIDTH - (camera.viewportWidth / 2));
+		camera.position.y = MathUtils.clamp(camera.position.y, camera.viewportHeight / 2,
+				TileMap.MAP_PIXEL_HEIGHT - (camera.viewportHeight / 2));
+
+//		if (player.getX() < camera.viewportWidth / 2) {
+//			camera.position.x = camera.viewportWidth / 2;
+//		} else if (player.getX() > TileMap.MAP_PIXEL_WIDTH - (camera.viewportWidth / 2)) {
+//			camera.position.x = TileMap.TILE_PIXEL_WIDTH - (camera.viewportWidth / 2);
+//		} else {
+//
+//			System.out.println("moveX");
+//
+//			camera.position.x = player.getX();
+//		}
+//
+//		if (player.getY() < camera.viewportHeight / 2) {
+//			camera.position.y = camera.viewportHeight / 2;
+//		} else if (player.getY() > TileMap.MAP_PIXEL_HEIGHT - (camera.viewportHeight / 2)) {
+//			camera.position.y = TileMap.TILE_PIXEL_HEIGHT - (camera.viewportHeight / 2);
+//		} else {
+//
+//			System.out.println("move y");
+//			camera.position.y = player.getY();
+//		}
+
 		camera.update();
 
 		// render()
@@ -101,13 +121,12 @@ public class GameScreen implements Screen {
 		tiledMapRenderer.setView(camera);
 		tiledMapRenderer.render();
 
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		player.draw(batch, 1);
-		batch.end();
+		spriteBatch.setProjectionMatrix(camera.combined);
+		spriteBatch.begin();
+		player.draw(spriteBatch);
+		spriteBatch.end();
 
 	}
-
 
 	@Override
 	public void resume() {
