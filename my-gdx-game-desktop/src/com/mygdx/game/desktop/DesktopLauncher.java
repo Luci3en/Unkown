b366b888c31894b99bbf62c8f3f29d19a0e70038
Luci3n <lukasries@t-online.de>
2018-05-19 01:32:47 +0200

    Revert "***"
    This reverts commit 317c72ec47cb73f40df814f13e535987af656b67.

--- my-gdx-game-desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ my-gdx-game-desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import game.Application;
-https://www.youtube.com/watch?v=KAS9ZCLzf1s
+
 public class DesktopLauncher {
 	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
